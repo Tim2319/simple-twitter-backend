02a00e46652bf152e36905bd24ce4cde990dac7b
Tim2319 <destiny65740@gmail.com>
2025-02-23 09:33:54 +0800

    feat: add seeder for public chatroom

---
+++ seeders/20250214074518-chatRooms.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('ChatRooms', [
+      {
+        isGroup: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('ChatRooms', null, {
+      where: {},
+      truncate: true
+    })
+  }
+}
