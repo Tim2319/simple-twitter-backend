192b378349070c6fd0789ceb57fd98eadb07104a
Tim2319 <destiny65740@gmail.com>
2025-03-18 17:14:57 +0800

    feat: add full Swagger API documentation and integration

--- route/index.js
+++ route/index.js
@@ -4,11 +4,13 @@ const users = require('./modules/users')
 const posts = require('./modules/posts')
 const admins = require('./modules/admin')
 const rooms = require('./modules/rooms')
+const swagger = require('./modules/swagger')
 
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
 // 總體路由
 router.use('/users', users)
+router.use('/swagger', swagger)
 router.use('/posts', authenticated, posts)
 router.use('/admin', authenticated, authenticatedAdmin, admins)
 router.use('/rooms', authenticated, rooms)
